6b64f4cb9ff19de72883629ad994c1e4a3ed011c
Eric Pascolo <eric.pascolo@gmail.com>
2020-12-14 15:43:54 +0100

    add function to create id map

--- lib/simulator/dataset.py
+++ lib/simulator/dataset.py
@@ -3,10 +3,12 @@ import numpy as np
 def init_dataset(param):
     mc_dataset = {}
     mc_dataset['rank'] = param["rank"]
-    mc_dataset['rank_map'] =  get_rank_map(param)  #i_end e i_start di ogni processore
-    i_start = mc_dataset['rank_map'][param['rank']]['i_start'] 
-    i_end = mc_dataset['rank_map'][param['rank']]['i_end']
-    n_local = i_end - i_start
+    
+    rank_map,n_local,id_array = id_map(param)
+
+    mc_dataset['rank_map'] = rank_map 
+    mc_dataset['id'] = id_array 
+    
     mc_dataset['alpha'] = np.empty(n_local)
     mc_dataset['beta'] = np.empty(n_local)
     mc_dataset['mu'] = np.empty(n_local)
@@ -14,9 +16,17 @@ def init_dataset(param):
     mc_dataset['t'] = []
     mc_dataset['n_it'] = param['n']
     mc_dataset['n_local'] = n_local
-    mc_dataset['id'] = np.arange(i_start, i_end )
     mc_dataset['bootstrap'] = []
     return mc_dataset
+
+def id_map(param):
+
+    rank_map =  get_rank_map(param)  
+    i_start = rank_map[param['rank']]['i_start'] 
+    i_end = rank_map[param['rank']]['i_end']
+    n_local = i_end - i_start
+    id_array = np.arange(i_start, i_end )
+    return [rank_map,n_local,id_array]
 
 def get_rank_map(param):
     n = param["n"]
