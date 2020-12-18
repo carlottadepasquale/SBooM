4d82e371241c6d99b28de7cada1e8d5159993dd4
Carlotta <carlotta.mdp@gmail.com>
2020-12-18 17:31:23 +0100

    bootstrap alg part 2 of many

--- lib/simulator/bootstrap.py
+++ lib/simulator/bootstrap.py
@@ -38,81 +38,116 @@ def bootstrap(param, dataset, comm):
     
 def confidence_int_1(param, dataset, comm):
     
-    q1alpha = np.quantile(dataset['bootstrap'][0]['alpha'], 0.05)
-    q1beta = np.quantile(dataset['bootstrap'][0]['beta'], 0.05)
-    q1mu = np.quantile(dataset['bootstrap'][0]['mu'], 0.05)
+    dataset['cint_alpha_1'] = np.empty(n_local)
+    dataset['cint_beta_1'] = np.empty(n_local)
+    dataset['cint_mu_1'] = np.empty(n_local)
+    
+    i_loc = 0
 
-    q2alpha = np.quantile(dataset['bootstrap'][0]['alpha'], 0.95)
-    q2beta = np.quantile(dataset['bootstrap'][0]['beta'], 0.95)
-    q2mu = np.quantile(dataset['bootstrap'][0]['mu'], 0.95)
+    for i in dataset['id']:
+        q1alpha = np.quantile(dataset['bootstrap'][i_loc]['alpha'], 0.05)
+        q1beta = np.quantile(dataset['bootstrap'][i_loc]['beta'], 0.05)
+        q1mu = np.quantile(dataset['bootstrap'][i_loc]['mu'], 0.05)
+
+        q2alpha = np.quantile(dataset['bootstrap'][i_loc]['alpha'], 0.95)
+        q2beta = np.quantile(dataset['bootstrap'][i_loc]['beta'], 0.95)
+        q2mu = np.quantile(dataset['bootstrap'][i_loc]['mu'], 0.95)
+
+        i_loc += 1
 
     print('cint1 a: ', q1alpha, q2alpha)
     print('cint1 b: ', q1beta, q2beta)
     print('cint1 m: ', q1mu, q2mu)
 
 
+
 def confidence_int_2(param, dataset, comm):
+    
+    dataset['cint_alpha_2'] = np.empty(n_local)
+    dataset['cint_beta_2'] = np.empty(n_local)
+    dataset['cint_mu_2'] = np.empty(n_local)
 
-    q1alpha = np.quantile(dataset['bootstrap'][0]['alpha'], 0.05)
-    q1beta = np.quantile(dataset['bootstrap'][0]['beta'], 0.05)
-    q1mu = np.quantile(dataset['bootstrap'][0]['mu'], 0.05)
+    for i in dataset['id']:
+        q1alpha = np.quantile(dataset['bootstrap'][0]['alpha'], 0.05)
+        q1beta = np.quantile(dataset['bootstrap'][0]['beta'], 0.05)
+        q1mu = np.quantile(dataset['bootstrap'][0]['mu'], 0.05)
 
-    q2alpha = np.quantile(dataset['bootstrap'][0]['alpha'], 0.95)
-    q2beta = np.quantile(dataset['bootstrap'][0]['beta'], 0.95)
-    q2mu = np.quantile(dataset['bootstrap'][0]['mu'], 0.95)
+        q2alpha = np.quantile(dataset['bootstrap'][0]['alpha'], 0.95)
+        q2beta = np.quantile(dataset['bootstrap'][0]['beta'], 0.95)
+        q2mu = np.quantile(dataset['bootstrap'][0]['mu'], 0.95)
 
-    cint2alpha = [2*dataset['alpha'][0]-q2alpha, 2*dataset['alpha'][0]-q1alpha]
-    cint2beta = [2*dataset['beta'][0]-q2beta, 2*dataset['beta'][0]-q1beta]
-    cint2mu = [2*dataset['mu'][0]-q2mu, 2*dataset['mu'][0]-q1mu]
+        cint2alpha = [2*dataset['alpha'][0]-q2alpha, 2*dataset['alpha'][0]-q1alpha]
+        cint2beta = [2*dataset['beta'][0]-q2beta, 2*dataset['beta'][0]-q1beta]
+        cint2mu = [2*dataset['mu'][0]-q2mu, 2*dataset['mu'][0]-q1mu]
 
     print("alpha cint 2: ", cint2alpha)
     print("beta cint 2: ", cint2beta)
     print("mu cint 2: ", cint2mu)
-    
 
+    
 def confidence_int_3(param, dataset, comm):
+    
+    dataset['cint_alpha_3'] = np.empty(n_local)
+    dataset['cint_beta_3'] = np.empty(n_local)
+    dataset['cint_mu_3'] = np.empty(n_local)
     
     stderr_a_bt, stderr_b_bt, stderr_m_bt = stderr_calc(param, dataset, comm)
 
-    alpha_cint = [dataset['alpha'][0] - 1.96*stderr_a_bt, dataset['alpha'][0] + 1.96*stderr_a_bt]
-    beta_cint = [dataset['beta'][0] - 1.96*stderr_b_bt, dataset['beta'][0] + 1.96*stderr_b_bt]
-    mu_cint = [dataset['mu'][0] - 1.96*stderr_m_bt, dataset['mu'][0] + 1.96*stderr_m_bt]
+    i_loc = 0
+    for i in dataset['id']:
+        alpha_cint = [dataset['alpha'][i_loc] - 1.96*stderr_a_bt[i_loc], dataset['alpha'][i_loc] + 1.96*stderr_a_bt[i_loc]]
+        beta_cint = [dataset['beta'][i_loc] - 1.96*stderr_b_bt[i_loc], dataset['beta'][i_loc] + 1.96*stderr_b_bt[i_loc]]
+        mu_cint = [dataset['mu'][i_loc] - 1.96*stderr_m_bt[i_loc], dataset['mu'][i_loc] + 1.96*stderr_m_bt[i_loc]]
+        i_loc += 1
 
     print("alpha cint 3: ", alpha_cint)
     print("beta cint 3: ", beta_cint)
     print("mu cint 3: ", mu_cint)
 
 
-
 def confidence_int_4(param, dataset, comm):
+
+    dataset['cint_alpha_4'] = np.empty(n_local)
+    dataset['cint_beta_4'] = np.empty(n_local)
+    dataset['cint_mu_4'] = np.empty(n_local)
 
     stderr_a_bt, stderr_b_bt, stderr_m_bt = stderr_calc(param, dataset, comm)
 
-    standardized_alpha_bt = (dataset['bootstrap'][0]['alpha']-dataset['alpha'][0])/stderr_a_bt
-    standardized_beta_bt = (dataset['bootstrap'][0]['beta']-dataset['beta'][0])/stderr_b_bt
-    standardized_mu_bt = (dataset['bootstrap'][0]['mu']-dataset['mu'][0])/stderr_m_bt
+    i_loc = 0
 
-    q1alpha = np.quantile(standardized_alpha_bt, 0.05)
-    q1beta = np.quantile(standardized_beta_bt, 0.05)
-    q1mu = np.quantile(standardized_mu_bt, 0.05)
+    for i in dataset['id']:
+        standardized_alpha_bt = (dataset['bootstrap'][i_loc]['alpha']-dataset['alpha'][i_loc])/stderr_a_bt[i_loc]
+        standardized_beta_bt = (dataset['bootstrap'][0]['beta']-dataset['beta'][0])/stderr_b_bt[i_loc]
+        standardized_mu_bt = (dataset['bootstrap'][0]['mu']-dataset['mu'][0])/stderr_m_bt[i_loc]
 
-    q2alpha = np.quantile(standardized_alpha_bt, 0.95)
-    q2beta = np.quantile(standardized_beta_bt, 0.95)
-    q2mu = np.quantile(standardized_mu_bt, 0.95)
+        q1alpha = np.quantile(standardized_alpha_bt[i_loc], 0.05)
+        q1beta = np.quantile(standardized_beta_bt[i_loc], 0.05)
+        q1mu = np.quantile(standardized_mu_bt[i_loc], 0.05)
 
-    cint4alpha = [dataset['alpha'][0]-q2alpha*stderr_a_bt, dataset['alpha'][0]-q1alpha*stderr_a_bt]
-    cint4beta = [dataset['beta'][0]-q2beta*stderr_b_bt, dataset['beta'][0]-q1beta*stderr_b_bt]
-    cint4mu = [dataset['mu'][0]-q2mu*stderr_m_bt, dataset['mu'][0]-q1mu*stderr_m_bt]
+        q2alpha = np.quantile(standardized_alpha_bt[i_loc], 0.95)
+        q2beta = np.quantile(standardized_beta_bt[i_loc], 0.95)
+        q2mu = np.quantile(standardized_mu_bt[i_loc], 0.95)
+
+        cint4alpha = [dataset['alpha'][0]-q2alpha*stderr_a_bt, dataset['alpha'][0]-q1alpha*stderr_a_bt]
+        cint4beta = [dataset['beta'][0]-q2beta*stderr_b_bt, dataset['beta'][0]-q1beta*stderr_b_bt]
+        cint4mu = [dataset['mu'][0]-q2mu*stderr_m_bt, dataset['mu'][0]-q1mu*stderr_m_bt]
+
+        i_loc += 1
 
     print("alpha cint 4: ", cint4alpha)
     print("beta cint 4: ", cint4beta)
     print("mu cint 4: ", cint4mu)
 
-
     
 def stderr_calc(param, dataset, comm):
-    stderr_a_bt = np.std(dataset['bootstrap'][0]['alpha'], ddof=1) / np.sqrt(param['bt'])
-    stderr_b_bt = np.std(dataset['bootstrap'][0]['beta'], ddof=1) / np.sqrt(param['bt'])
-    stderr_m_bt = np.std(dataset['bootstrap'][0]['mu'], ddof=1) / np.sqrt(param['bt'])
+    i_loc = 0
+    for i in dataset['id']:
+        stderr_a_bt = np.empty(dataset['n_local'])
+        stderr_b_bt = np.empty(dataset['n_local'])
+        stderr_m_bt = np.empty(dataset['n_local'])
+        stderr_a_bt[i_loc] = np.std(dataset['bootstrap'][i_loc]['alpha'], ddof=1) / np.sqrt(param['bt'])
+        stderr_b_bt[i_loc] = np.std(dataset['bootstrap'][i_loc]['beta'], ddof=1) / np.sqrt(param['bt'])
+        stderr_m_bt[i_loc] = np.std(dataset['bootstrap'][i_loc]['mu'], ddof=1) / np.sqrt(param['bt'])
+        i_loc += 1
 
     return [stderr_a_bt, stderr_b_bt, stderr_m_bt]
