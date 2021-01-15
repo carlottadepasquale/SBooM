7ba0e1b6d2578e6ad68f1430b017e9431fda4188
Carlotta <carlotta.mdp@gmail.com>
2021-01-15 16:45:07 +0100

    no print

--- lib/Hawkes/tools/Quasi_Newton.py
+++ lib/Hawkes/tools/Quasi_Newton.py
@@ -172,10 +172,10 @@ def Quasi_Newton(model,prior=[],merge=[],opt=[]):
         with np.errstate(invalid='raise'):
             try:
                 ste = EstimationError(model,para,prior)
-                print('QN ste:', ste)
+                #print('QN ste:', ste)
             except FloatingPointError:
                 count_err += 1
-                print('QN count_err:', count_err)
+                #print('QN count_err:', count_err)
                 
 
     ###OPTION: Check map solution
